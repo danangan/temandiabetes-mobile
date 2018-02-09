b363813aee370dcac73499828b88b9fb440bc521
Eri Irawan <eri@Administrators-MacBook-Air.local>
2018-02-09 15:47:31 +0700

    app initialized

---
+++ src/utils/appInitialized.js
@@ -0,0 +1,16 @@
+import { AsyncStorage } from 'react-native';
+
+import { startApp, mainApp } from '../../App';
+import { authToken } from '../utils/constants';
+
+const appInitialized = async () => {
+	const token = await AsyncStorage.getItem(authToken);
+	
+	if (!token) {
+		startApp();
+	} else {
+		mainApp();
+	}
+};
+
+export default appInitialized;
