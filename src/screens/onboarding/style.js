624ae1ba874d00823201daf77f391116248c60ab
Eri Irawan <eri@Administrators-MacBook-Air.local>
2018-02-09 15:46:01 +0700

    skeleton style

---
+++ src/screens/onboarding/style.js
@@ -0,0 +1,17 @@
+import color from '../../style/color';
+
+const styles = {
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: color.pink
+	},
+	text: {
+		color: '#fff',
+		fontSize: 30,
+		fontWeight: '500',
+	},
+};
+
+export default styles;
