bd8062b8f7effa4633ffd25bbad0d8ed8e35096b
Eri Irawan <eri@Administrators-MacBook-Air.local>
2018-02-09 15:47:10 +0700

    default config text

---
+++ src/style/text.js
@@ -0,0 +1,49 @@
+const text = {
+	p: {
+		color: 'black',
+    // fontFamily: 'Open Sans',
+    fontSize: 14
+  },
+  title: {
+    color: 'black',
+    // fontFamily: 'Open Sans',
+    fontSize: 20
+  },
+  number: {
+    color: 'black',
+    // fontFamily: 'Open Sans',
+    fontSize: 20
+  },
+  button: {
+    color: 'white',
+    // fontFamily: 'Open Sans',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  subTitle: {
+    color: 'black',
+    // fontFamily: 'Open Sans',
+    fontSize: 14
+  },
+  sectionTitle: {
+    color: 'black',
+    // fontFamily: 'Open Sans',
+    fontSize: 16,
+    fontWeight: '700'
+  },
+  shadow: {
+    shadowOffset: {
+      width: 1,
+      height: 1
+    },
+    shadowColor: 'black',
+    shadowOpacity: 1
+  },
+  labelText: {
+    color: 'white',
+    // fontFamily: 'Open Sans',
+    fontSize: 10
+  }
+};
+
+export default text;
