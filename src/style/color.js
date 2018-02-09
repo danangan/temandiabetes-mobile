e5425396041aea3269a8d4d854e166a5678bee43
Eri Irawan <eri@Administrators-MacBook-Air.local>
2018-02-09 15:46:16 +0700

    default colors

---
+++ src/style/color.js
@@ -0,0 +1,14 @@
+const color = {
+  black: 'rgb(0,0,0)',
+  gray: 'rgb(102,102,102)',
+  midGray: 'rgb(169,169,169)',
+  lightGray: 'rgb(235,235,235)',
+  navbarGray: 'rgb(246,246,246)',
+  pink: 'rgb(255,127,124)',
+  green: 'rgb(148,235,95)',
+  flora: 'rgb(102,255,102)',
+  white: '#ffffff',
+  yellow: 'rgb(255,204,102)',
+  aqua: 'rgb(102,153,255)'
+};
+export default color;
