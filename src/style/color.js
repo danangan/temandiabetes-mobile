56345901e619fd0f4006efbcf2aaa564c340c357
Eri Irawan <eri@Administrators-MacBook-Air.local>
2018-02-11 16:42:03 +0700

    default colors

--- src/style/color.js
+++ src/style/color.js
@@ -1,10 +1,10 @@
 const color = {
   black: 'rgb(0,0,0)',
   gray: 'rgb(102,102,102)',
+  gray1: 'rgb(225, 232, 238)', 
   midGray: 'rgb(169,169,169)',
-  white: '#ffffff',
-  amaranth: 'rgb(239, 67, 79)',
-  deepKoamaru: 'rgb(37, 44, 104)',
-  solitude: 'rgb(239, 242, 253)'
+  white: 'rgb(239, 242, 253)',
+  red: 'rgb(239, 67, 79)',
+  blue: 'rgb(37, 44, 104)',
 };
 export default color;
