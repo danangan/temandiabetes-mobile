b83674822a86027398f4d509c5e10ec128a8bb98
Eri Irawan <eri@Administrators-MacBook-Air.local>
2018-02-22 20:58:07 +0700

    wiring with redux, oAuth firebase for register

--- src/actions/registerActions.js
+++ src/actions/registerActions.js
@@ -1,38 +1,44 @@
+import firebase from 'react-native-firebase';
+import axios from 'axios';
+
 import * as ActionTypes from './constants';
-import axios from 'axios'
-import { API_SIGN_UP, API_SIGN_IN } from "../utils/API";
+import { API_SIGN_UP, API_SIGN_IN } from '../utils/API';
 
-const signAction = (idToken) => {
-  firebase.auth().signInWithCustomToken(token).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
+const signAction = idToken => {
+	firebase
+		.auth()
+		.signInWithCustomToken(token)
+		.catch((error) => {
+			// Handle Errors here.
+			const errorCode = error.code;
+			const errorMessage = error.message;
 
-}
-
-const registerActionSuccess = (data) => ({
-  type: ActionTypes.REGISTER_USER,
-  payload: data
-});
-
-const registerAction = (userData) => {
-  console.log("ADAA... ", userData);
-  return (dispatch) => {
-    axios.post(API_SIGN_UP, userData)
-      .then(data => {
-        dispatch(registerActionSuccess(data));
-        dispatch(signAction(data.idToken));
-      })
-      .catch(error => dispatch(registerActionSuccess(error)));
-  };
+			console.log('TOKEN : ', token, idToken);
+			// ...
+		});
 };
 
-const registerStepOne = (data) => ({
-  type: ActionTypes.REGISTER_STEP_ONE,
-  payload: data
+const registerActionSuccess = data => ({
+	type: ActionTypes.REGISTER_USER,
+	payload: data
+});
+
+const registerAction = userData => {
+	console.log('ADAA... ', userData);
+	return dispatch => {
+		axios
+			.post(API_SIGN_UP, userData)
+			.then(data => {
+				dispatch(registerActionSuccess(data));
+				dispatch(signAction(data.idToken));
+			})
+			.catch(error => dispatch(registerActionSuccess(error)));
+	};
+};
+
+const registerStepOne = data => ({
+	type: ActionTypes.REGISTER_STEP_ONE,
+	payload: data
 });
 
 export { registerAction, registerStepOne };
-
