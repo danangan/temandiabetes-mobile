c208d4b57db257254aa721fb36c4e0873004a40a
Eri Irawan <eri@Administrators-MacBook-Air.local>
2018-02-27 08:35:39 +0700

    screen tabar

---
+++ src/screens/emergency/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Emergency extends Component {
+	render() {
+		return (
+			<View>
+				<Text>EMERGENCY</Text>
+			</View>
+		);
+	}
+}
+
+export default Emergency;
