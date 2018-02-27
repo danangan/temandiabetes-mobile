c629f4c849809c7513eabda19e5035f77e0e5d99
Eri Irawan <eri@Administrators-MacBook-Air.local>
2018-02-28 01:47:27 +0700

    modified style

--- src/screens/register/style.js
+++ src/screens/register/style.js
@@ -1,53 +1,64 @@
-import { StyleSheet  } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 3,
 		backgroundColor: '#ccc',
-		justifyContent: 'space-between',
+		justifyContent: 'space-between'
 	},
 	imageBackground: {
 		flex: 1,
-		zIndex:100,
+		zIndex: 100,
 		width: '100%',
 		height: '100%',
 		flexDirection: 'column',
 		justifyContent: 'center',
-		alignItems: 'center',
+		alignItems: 'center'
+	},
+	buttonText: {
+		color: '#fff',
+		fontFamily: 'Montserrat-Bold'
 	},
 	wrapTitle: {
 		flex: 2,
 		justifyContent: 'center',
-		width: '90%',
+		width: '90%'
 	},
 	titles: {
 		color: '#4a4a4a',
 		fontSize: 32,
 		paddingRight: 50,
-    fontFamily: 'Montserrat-Regular'
+		fontFamily: 'Montserrat-Regular'
 	},
 	wrapForm: {
 		flex: 2,
 		width: '90%',
-		justifyContent: 'center',
+		justifyContent: 'center'
 	},
-  textInputStyle: {
-    backgroundColor: '#fff',
-    color: '#ccc',
-    height: 50,
-    borderRadius: 5,
-  },
-  btnNext: {
-    backgroundColor: '#ef434f',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 40,
-  },
+	textInputStyle: {
+		backgroundColor: '#fff',
+		color: '#ccc',
+		height: 50,
+		borderRadius: 5,
+		fontFamily: 'Montserrat-Bold'
+	},
+	btnNext: {
+		backgroundColor: '#ef434f',
+		alignItems: 'center',
+		justifyContent: 'center',
+		height: 40
+	},
 	indicatorWrapper: {
 		height: '30%',
 		marginBottom: 10,
 		justifyContent: 'center',
 		alignItems: 'center'
+	},
+	wrapIconBack: {
+		flex: 1,
+		justifyContent: 'flex-start',
+		alignItems: 'flex-start',
+		alignSelf: 'flex-start'
 	}
 });
 
