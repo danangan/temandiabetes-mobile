80cb88bd3da78a588f68b235c7f503e6639be188
Eri Irawan <eri@Administrators-MacBook-Air.local>
2018-03-09 11:39:20 +0700

    clean code & change image background for onboarding screens

--- src/screens/onboarding/Screen.js
+++ src/screens/onboarding/Screen.js
@@ -1,74 +1,35 @@
 import React, { Component } from 'react';
-import {
-	StyleSheet, // CSS-like styles
-	Text, // Renders text
-	View,
-	ImageBackground
-	// Container component
-} from 'react-native';
+import { StyleSheet, Text, ImageBackground } from 'react-native';
 
-import Button from './Button';
 import Swipper from './Swipper';
-import backImage from '../../assets/images/first_onboarding.jpg';
-import backImage2 from '../../assets/images/second_onboarding.jpg';
-import backImage3 from '../../assets/images/siapakah_anda.jpg';
+import firstOnBoarding from '../../assets/images/onboarding_rekam.png';
+import secondOnBoarding from '../../assets/images/onboarding_forum.png';
+import thirdOnBoarding from '../../assets/images/onboarding_event.png';
+import Style from '../../style/defaultStyle';
 
 class Screen extends Component {
 	render() {
 		return (
 			<Swipper navigation={this.props.navigation}>
 				{/* First screen */}
-				<ImageBackground
-					style={{
-						flex: 1,
-						zIndex: 100,
-						width: '100%',
-						height: '100%',
-						flexDirection: 'column',
-						justifyContent: 'center',
-						alignItems: 'flex-start'
-					}}
-					source={backImage}
-				>
-					<Text style={styles.header}>Rekam Data Anda</Text>
-					<Text style={styles.text}>
+				<ImageBackground style={styles.imageBackgroundStyle} source={firstOnBoarding}>
+					<Text style={styles.headerStyle}>Rekam Data{'\n'}Anda</Text>
+					<Text style={styles.textStyle}>
 						Pantau gula darah secara instan menggunakan
 						glukometer langsung dari gadget Anda.
 					</Text>
 				</ImageBackground>
 				{/* First screen */}
-				<ImageBackground
-					style={{
-						flex: 1,
-						zIndex: 100,
-						width: '100%',
-						height: '100%',
-						flexDirection: 'column',
-						justifyContent: 'center',
-						alignItems: 'flex-start'
-					}}
-					source={backImage2}
-				>
-					<Text style={styles.header}>Forum</Text>
-					<Text style={styles.text}>
+				<ImageBackground style={styles.imageBackgroundStyle} source={secondOnBoarding}>
+					<Text style={styles.headerStyle}>Forum</Text>
+					<Text style={styles.textStyle}>
 						Konsultasikan kesehatan Anda dengan ahlinya	
 					</Text>
 				</ImageBackground>
 				{/* Third screen */}
-				<ImageBackground
-					style={{
-						flex: 1,
-						zIndex: 100,
-						width: '100%',
-						height: '100%',
-						flexDirection: 'column',
-						justifyContent: 'center',
-						alignItems: 'flex-start'
-					}}
-					source={backImage3}
-				>
-					<Text style={styles.header}>Event</Text>
-					<Text style={styles.text}>
+				<ImageBackground style={styles.imageBackgroundStyle} source={thirdOnBoarding}>
+					<Text style={styles.headerStyle}>Event</Text>
+					<Text style={styles.textStyle}>
 						Dapatkan jadwal event menarik seputar kesehatan
 						bersama Teman Diabetes
 					</Text>
@@ -81,29 +42,26 @@ class Screen extends Component {
 export default Screen;
 
 const styles = StyleSheet.create({
-	// Slide styles
-	slide: {
-		flex: 1, // Take up all screen
-		justifyContent: 'center', // Center vertically
-		alignItems: 'flex-start',
-		marginHorizontal: 20
-	},
-	// Header styles
-	header: {
-		color: '#4A4A4A',
-		fontFamily: 'Avenir',
-		fontSize: 30,
-		fontWeight: 'bold',
+	headerStyle: {
+		fontFamily: 'Montserrat-Regular',
+		fontSize: Style.FONT_SIZE_TITLE * 1.4,
 		paddingHorizontal: 20,
 		marginTop: 200
 	},
-	// Text below header
-	text: {
-		color: '#4A4A4A',
-		fontFamily: 'Avenir',
-		fontSize: 18,
+	textStyle: {
+		fontFamily: 'Montserrat-Regular',
+		fontSize: Style.FONT_SIZE,
 		textAlign: 'left',
 		marginVertical: 10,
 		paddingHorizontal: 20
+	},
+	imageBackgroundStyle: {
+		flex: 1,
+		zIndex: 100,
+		width: Style.DEVICE_WIDTH,
+		height: Style.DEVICE_HEIGHT,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'flex-start'
 	}
 });
