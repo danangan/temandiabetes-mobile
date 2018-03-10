4defd39610457ed6d0f8c4b20e04d5c5816ee123
Eri Irawan <eri@Administrators-MacBook-Air.local>
2018-03-10 10:42:28 +0700

    Textfield seacrh and post threads

--- src/screens/forum/tab-home/index.js
+++ src/screens/forum/tab-home/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { View, ScrollView, Platform, TouchableOpacity } from 'react-native';
-import { Card, FooterThread, HeaderThread } from '../../../components';
+import { View, Platform, TouchableOpacity, FlatList, TextInput } from 'react-native';
+
+import { Card, FooterThread, HeaderThread, TextField, CardSection } from '../../../components';
 import ContentThread from './contentThread';
+import searchIcon from '../../../assets/icons/close.png';
+import Blood from '../../../assets/icons/explorer_icon.png';
 
 class TabHome extends Component {
 	static navigatorStyle = {
@@ -16,33 +19,94 @@ class TabHome extends Component {
 		};
 	}
 
-	render() {
+	renderHeader() {
 		return (
 			<View>
-				<ScrollView>
-					{this.state.nums.map((a, index) => (
-						<TouchableOpacity
-							onPress={() =>
-								this.props.navigator.push({
-									screen: 'TemanDiabets.ThreadDetails',
-									navigatorStyle: {
-										navBarHidden: true
-									}
-								})
-							}
-						>
-							<Card key={index} containerStyle={styles.container}>
-								<HeaderThread
-									source="http://s3.amazonaws.com/systemgravatars/avatar_6225.jpg"
-									name='Gloria James'
-									category='Teman Diabetes'
-								/>
-								<ContentThread />
-								<FooterThread numOfComments={17} />
-							</Card>
-						</TouchableOpacity>
-					))}
-				</ScrollView>
+				{/* <CardSection> */}
+					<TextField
+						leftIcon={Blood}
+						rightIcon={searchIcon}
+						placeholder={'Cari post, pengguna'}
+						underlineColorAndroid={'#fff'}
+						sectionStyle={{
+							backgroundColor: 'yellow',
+							borderWidth: 1,
+							borderColor: '#fff',
+							borderRadius: 5
+						}}
+						inputStyle={{ color: '#b6b6b6', fontSize: 12, backgroundColor: 'red' }}
+					/>
+				{/* </CardSection> */}
+				{/* <TextField
+					placeholder={'Tanya atau bagikan disini'}
+					containerStyle={{
+						backgroundColor: '#ccc',
+						borderWidth: 1,
+						borderColor: '#fff',
+						borderRadius: 5,
+						marginHorizontal: 5,
+						height: 70
+					}}
+					underlineColorAndroid="rgba(0,0,0,0)"
+					inputStyle={{ color: '#b6b6b6', fontSize: 12, height: 100, backgroundColor: 'red' }}
+				/> */}
+				<View
+					style={{
+						backgroundColor: 'white',
+						borderWidth: 1,
+						borderColor: '#fff',
+						borderRadius: 5,
+						marginHorizontal: 5,
+						elevation: 2.5,
+						height: 70
+					}}
+				>
+					<TextInput
+						style={{
+							fontSize: 16,
+							height: 70
+						}}
+						placeholder="Tanya atau bagiakn disini"
+						underlineColorAndroid="rgba(0,0,0,0)"
+					/>
+				</View>
+			</View>
+		);
+	}
+
+	renderItem(item) {
+		return (
+			<TouchableOpacity
+				onPress={() =>
+					this.props.navigator.push({
+						screen: 'TemanDiabets.ThreadDetails',
+						navigatorStyle: {
+							navBarHidden: true
+						}
+					})
+				}
+			>
+				<Card containerStyle={styles.container}>
+					<HeaderThread
+						source="http://s3.amazonaws.com/systemgravatars/avatar_6225.jpg"
+						name="Gloria James"
+						category="Teman Diabetes"
+					/>
+					<ContentThread />
+					<FooterThread numOfComments={17} />
+				</Card>
+			</TouchableOpacity>
+		);
+	}
+
+	render() {
+		return (
+			<View style={{ flex: 1 }}>
+				<FlatList
+					ListHeaderComponent={() => this.renderHeader()}
+					data={this.state.nums}
+					renderItem={item => this.renderItem(item)}
+				/>
 			</View>
 		);
 	}
