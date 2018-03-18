1bcf4291ed750234506bbd740fde87ad8e589e93
Daniel Agus <danielagus@Daniels-MacBook-Pro.local>
2018-03-18 23:33:25 +0700

    API base constants

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,4 +1,5 @@
 // import { Config } from 'react-native-config';
 
+export const API_BASE_DEVELOPMENT = 'https://development-dot-temandiabetes.appspot.com';
 export const API_SIGN_UP = 'https://development-dot-temandiabetes.appspot.com/api/sign-up';
 export const API_SIGN_IN = 'https://temandiabetes.ga/api/sign-in';
