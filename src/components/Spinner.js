30ef4f44b053f1bec95cc0f56af79eea63bed40e
Eri Irawan <eriirawan@Eris-MacBook-Pro.local>
2018-03-20 17:24:43 +0700

    add splash for loading

---
+++ src/components/Spinner.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+import Style from '../style/defaultStyle';
+
+const Spinner = ({ containerStyle, textStyle, text, color, size }) => (
+	<View style={[styles.containerStyle, containerStyle]}>
+		<ActivityIndicator color={color} size={size} />
+		<Text style={[styles.textStyle, textStyle]}>{text}</Text>
+	</View>
+);
+
+const styles = {
+	containerStyle: {
+		position: 'absolute',
+		flex: 1,
+		alignSelf: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#383838',
+		width: Style.DEVICE_WIDTH,
+		height: Style.DEVICE_HEIGHT,
+		alignItems: 'center',
+		borderRadius: 6,
+		opacity: 0.7
+	},
+	textStyle: {
+		fontFamily: 'Montserrat-Regular',
+		fontWeight: 'bold',
+		fontSize: 16,
+		color: '#FFFFFF'
+	}
+};
+
+export { Spinner };
