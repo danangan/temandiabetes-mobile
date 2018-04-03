d95972802a89a12dac1955776b1351eca01bd1a0
Eri Irawan <eriirawan@Eris-MacBook-Pro.local>
2018-04-04 05:36:36 +0700

    add on navigation event fro burger menu and notif

--- src/screens/emergency/index.js
+++ src/screens/emergency/index.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class Emergency extends Component {
+	constructor(props) {
+		super(props);
+		this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+	}
+
+	onNavigatorEvent(event) {
+		if (event.type === 'NavBarButtonPress') {
+			if (event.id === 'notification') {
+				alert('NavBar', 'Edit button pressed');
+			}
+			if (event.id === 'sideMenu') {
+				alert('NavBar', 'Add button pressed');
+			}
+		}
+	}
+	
 	render() {
 		return (
 			<View>
--- src/screens/input-tracker/tab-input-tracker/index.js
+++ src/screens/input-tracker/tab-input-tracker/index.js
@@ -3,15 +3,31 @@ import { View, Text, AsyncStorage } from 'react-native';
 import { authToken } from '../../../utils/constants';
 
 class TabInputTracker extends Component {
+	constructor(props) {
+		super(props);
+		this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+	}
+
 	componentDidMount() {
 		// AsyncStorage.removeItem(authToken);
 		// this.getToken();
 	}
 
+	onNavigatorEvent(event) {
+		if (event.type === 'NavBarButtonPress') {
+			if (event.id === 'notification') {
+				alert('NavBar', 'Edit button pressed');
+			}
+			if (event.id === 'sideMenu') {
+				alert('NavBar', 'Add button pressed');
+			}
+		}
+	}
+
 	getToken = async () => {
 		const token = await AsyncStorage.getItem(authToken);
 		// console.log('Token: ', token);
-	}
+	};
 
 	render() {
 		return (
