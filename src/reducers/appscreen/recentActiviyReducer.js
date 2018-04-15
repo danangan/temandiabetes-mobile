b1ca45956d42560014fb3fc4d95188d33163d099
Daniel Agus <danielagus@Daniels-MacBook-Pro.local>
2018-04-16 01:25:58 +0700

    adding function reducer get recent comments

--- src/reducers/appscreen/recentActiviyReducer.js
+++ src/reducers/appscreen/recentActiviyReducer.js
@@ -15,11 +15,20 @@ const initialState = {
   }
 };
 
-function getUserRecentActivity(state, payload) {
+function getUserRecentThread(state, payload) {
   return {
     ...state,
     recentThreads: { 
       ...state.recentThreads, status_code: 201, data: payload 
+    }
+  };
+}
+
+function getUserRecentComment(state, payload) {
+  return {
+    ...state,
+    recentComments: {
+      ...state.recentComments, data: payload, status_code: 201
     }
   };
 }
@@ -34,7 +43,17 @@ const recentActivityReducer = (state = initialState, action) => {
         }
       };
 		case ActionTypes.GET_USER_RECENT_THREADS:
-			return getUserRecentActivity(state, action.payload);
+      return getUserRecentThread(state, action.payload);
+    case 'PENDING_GET_USER_RECENT_COMMENTS': {
+      return {
+        ...state, 
+        recentThreads: {
+          ...state.recentComments, status_code: 0, data: []
+        }
+      };
+    }
+    case ActionTypes.GET_USER_RECENT_COMMENTS: 
+      return getUserRecentComment(state, action.payload);
 		default:
 			return state;
 	}
