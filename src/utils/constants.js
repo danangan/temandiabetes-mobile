50bb7672226ac2e0b2920bda956b9c5c88cdf67f
Danang Arbansa Nur <danang.arbansa@ptdki.co.id>
2018-04-19 17:14:21 +0000

    Merged in global-current-user (pull request #26)
    Global current user
    
    * connect the card profile to current user in redux
    
    * add default profile image

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,6 @@ export const authToken = 'temandiabetes/token';
 export const buttonLabelNext = 'LANJUT';
 export const buttonLabelDone = 'SELESAI';
 export const typeOfUsers = ['diabetesi', 'non-diabetesi', 'ahli'];
+export const DEFAULT_PROFILE_IMAGE = 'https://images-cdn.9gag.com/photo/aMjGOVM_700b.jpg';
 export const URL_IMAGE =
 	'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAqDAAAAJDNhMmVjZTMxLWYwMmYtNDkyMS05MWQ5LTQ5NGY1ZDQzNDc4OA.jpg';
