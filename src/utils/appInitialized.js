273e443653941e9db41cfb455a588f9cf245f037
Danang Arbansa Nur <danang.arbansa@ptdki.co.id>
2018-04-21 09:24:48 +0000

    Merged in axios-wrapper (pull request #28)
    Axios wrapper
    
    * add axios wrapper and example in fetch static thread
    
    * fix the example
    
    * update the app initialize. change how check user authenticate using firebase

--- src/utils/appInitialized.js
+++ src/utils/appInitialized.js
@@ -1,15 +1,24 @@
 import { AsyncStorage } from 'react-native';
+import firebase from 'react-native-firebase';
 
 import { startApp, mainApp } from '../../App';
 import { authToken } from '../utils/constants';
 
+
+
 const appInitialized = async () => {
-	const token = await AsyncStorage.getItem(authToken);
-	if (!token) {
-		startApp();
-	} else {
-		mainApp();
-	}
+  startApp();
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      firebase.auth().currentUser.getIdToken().then((firebaseIdToken) => {
+        AsyncStorage.setItem(authToken, firebaseIdToken).then(() => {
+          mainApp();
+        })
+      })
+    }
+  })
+
 };
 
 export default appInitialized;
