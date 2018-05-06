622d8cf7b184996ef83b18115f24127acd017ad6
Eri Irawan <eri.irawan@ptdki.co.id>
2018-05-06 16:11:02 +0000

    Merged in menu-input-tracker (pull request #37)
    Menu screen for input tracker

---
+++ src/screens/input-tracker/tab-input-tracker/BloodPressure.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, TouchableOpacity, Text, Image } from 'react-native';
+
+import { CardSection } from '../../../components';
+import styles from './Styles';
+
+const bloodPressure = () => (
+  <TouchableOpacity>
+    <CardSection>
+      <Image
+        source={{
+          uri:
+            'https://is1-ssl.mzstatic.com/image/thumb/Purple128/v4/8b/3a/0a/8b3a0a25-603a-3374-90e9-067df014fd60/source/512x512bb.jpg'
+        }}
+        style={styles.iconStyle}
+      />
+      <View style={styles.containerTextStyle}>
+        <Text style={styles.titleStyle}>tekanan darah</Text>
+        <Text style={styles.valueStyle}>masukkan tekanan darah anda</Text>
+      </View>
+    </CardSection>
+  </TouchableOpacity>
+);
+
+export default bloodPressure;
--- src/screens/input-tracker/tab-input-tracker/index.js
+++ src/screens/input-tracker/tab-input-tracker/index.js
@@ -1,45 +1,113 @@
 import React, { Component } from 'react';
-import { View, Text, AsyncStorage } from 'react-native';
-import { authToken } from '../../../utils/constants';
+import { View, Platform, TouchableOpacity, Text } from 'react-native';
 
-class TabInputTracker extends Component {
-	constructor(props) {
-		super(props);
-		this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
-	}
+import color from '../../../style/color';
+import { Card, Button } from '../../../components';
+import MenuButton from './MenuButton';
+import Style from '../../../style/defaultStyle';
 
-	componentDidMount() {
-		// AsyncStorage.removeItem(authToken);
-		this.getToken();
-	}
+class InputTracker extends Component {
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+  }
 
-	onNavigatorEvent(event) {
-		if (event.type === 'NavBarButtonPress') {
-			if (event.id === 'notification') {
-				alert('Development')
-			}
-			if (event.id === 'sideMenu') {
-				this.props.navigator.showModal({
-					screen: 'TemanDiabets.ProfileScreen',
-					title: 'Modal',
-					animationType: 'none'
-				});
-			}
-		}
-	}
+  componentDidMount() {}
 
-	getToken = async () => {
-		const token = await AsyncStorage.getItem(authToken);
-		console.log('Token: ', token);
-	};
+  onNavigatorEvent(event) {
+    if (event.type === 'NavBarButtonPress') {
+      if (event.id === 'notification') {
+        alert('Development');
+      }
+      if (event.id === 'sideMenu') {
+        this.props.navigator.showModal({
+          screen: 'TemanDiabets.ProfileScreen',
+          title: 'Modal',
+          animationType: 'none'
+        });
+      }
+    }
+  }
 
-	render() {
-		return (
-			<View>
-				<Text>VIDEO</Text>
-			</View>
-		);
-	}
+  render() {
+    return (
+      <View style={styles.containerStyle}>
+        <Card containerStyle={styles.cardStyle}>
+          <MenuButton />
+        </Card>
+        <View style={styles.buttonContainerStyle}>
+          <Button buttonStyle={styles.buttonStyle} textStyle={styles.textButtonStyle}>
+            KIRIM
+          </Button>
+          <Button
+            buttonStyle={[styles.buttonStyle, styles.buttonReset]}
+            textStyle={[styles.textButtonStyle, styles.textReset]}
+          >
+            ATUR ULANG
+          </Button>
+        </View>
+      </View>
+    );
+  }
 }
 
-export default TabInputTracker;
+const styles = {
+  containerStyle: {
+    flex: 1,
+    backgroundColor: color.solitude
+  },
+  cardStyle: {
+    borderRadius: 15,
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    marginTop: 19.44,
+    marginLeft: 8.8,
+    marginRight: 8.8,
+    marginBottom: 22.59,
+    paddingLeft: 29.2,
+    paddingTop: 25.6,
+    paddingBottom: 24.85,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgba(0,0,0, .2)',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2
+      },
+      android: {
+        elevation: 0.05
+      }
+    })
+  },
+  buttonContainerStyle: {
+    alignSelf: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  buttonStyle: {
+    width: 125.41,
+    height: 46.95,
+    borderRadius: 0,
+    borderWidth: 2,
+    paddingVertical: 5,
+    borderColor: 'rgba(255,255,255,0.2)'
+  },
+  textButtonStyle: {
+    fontSize: Style.FONT_SIZE_SMALLER,
+    fontFamily: 'Montserrat-Regular',
+    color: color.white,
+    fontWeight: '900'
+  },
+  buttonReset: {
+    backgroundColor: color.white,
+    borderColor: color.red,
+    borderWidth: 1
+  },
+  textReset: {
+    color: color.red
+  }
+};
+
+export default InputTracker;
