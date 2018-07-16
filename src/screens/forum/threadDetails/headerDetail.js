a362a2d6b8d76a51390281866ffbaa951f9616de
Eri Irawan <eri.irawan@ptdki.co.id>
2018-07-16 15:19:18 +0000

    Merged in bugs-fixing (pull request #94)
    Bugs fixing
    
    * change wording in Apps
    
    * add component snack bar
    
    * implement component snack bar in login screen
    
    * deleted component toast
    
    * add snack bar, clean code
    
    * add snack bar
    
    * code style
    
    * change version name for apk :  0.1.49
    
    * bugs in register, typo for root project. use TemanDiabetes for next please!
    
    * handle add inner circle double
    
    * comment search product
    
    * add snackbar
    
    * accept request to inner circle from visit profile
    
    * after confirm request to inner circle, navigation pop to before screen
    
    * button laporkan dirubah jadi lapor, responsive for font size.

--- src/screens/forum/threadDetails/headerDetail.js
+++ src/screens/forum/threadDetails/headerDetail.js
@@ -3,21 +3,22 @@ import { View, Text } from 'react-native';
 
 import { CardSection, Avatar } from '../../../components';
 import { formatDateTime, capitalize } from '../../../utils/helpers';
+import Style from '../../../style/defaultStyle';
 
 class HeaderDetail extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
   renderCategory(item = '') {
-    let category = ''
+    let category = '';
     if (Array.isArray(item)) {
       if (item.length > 0) {
-        category = item[0] !== null ? item[0].category : category
+        category = item[0] !== null ? item[0].category : category;
       }
     } else {
-      category = item
+      category = item;
     }
     if (category !== '') {
       return (
@@ -28,50 +29,58 @@ class HeaderDetail extends React.Component {
     }
   }
 
-	render() {
+  render() {
     const { authorItem, categoryItem, date, threadType } = this.props;
-		return (
-			<CardSection containerStyle={{ backgroundColor: '#f2f4fd', margin: 0 }}>
-				<View style={styles.container}>
-					<Avatar
+    return (
+      <CardSection containerStyle={{ backgroundColor: '#f2f4fd', margin: 0 }}>
+        <View style={styles.container}>
+          <Avatar
             avatarSize="Small"
             userName={authorItem.nama === null ? 'Loading' : authorItem.nama}
-						imageSource={authorItem.foto_profile}
-					/>
-					<View style={{ flex: 1, margin: 5 }}>
-						<Text style={{ fontSize: 12 }}>{authorItem.nama === null ? 'Loading' : authorItem.nama}</Text>
-						<Text style={{ fontSize: 10 }}>Posted on {formatDateTime(date)}</Text>
+            imageSource={authorItem.foto_profile}
+          />
+          <View style={{ flex: 1, margin: 5 }}>
+            <Text style={{ fontSize: 12 }}>
+              {authorItem.nama === null ? 'Loading' : authorItem.nama}
+            </Text>
+            <Text style={{ fontSize: 10 }}>Posted on {formatDateTime(date)}</Text>
           </View>
           {this.renderCategory(threadType)}
           {this.renderCategory(categoryItem)}
-				</View>
-			</CardSection>
-		);
-	}
+        </View>
+      </CardSection>
+    );
+  }
 }
 
 const styles = {
-	container: {
-		flex: 1,
-		flexDirection: 'row',
-		alignItems: 'flex-end',
-		justifyContent: 'flex-end',
-		paddingTop: 15,
-		paddingHorizontal: 15,
-		borderRadius: 15
-	},
-	wrapperButton: {
-		marginVertical: 5,
-		marginHorizontal: 1,
-		alignItems: 'center',
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    paddingTop: 15,
+    paddingHorizontal: 15,
+    borderRadius: 15
+  },
+  wrapperButton: {
+    marginVertical: 5,
+    marginHorizontal: 1,
+    alignItems: 'center',
     backgroundColor: '#252c68',
-    borderRadius: 10,
-	},
-	titleButton: {
-		fontSize: 12,
-		color: '#8084a7',
-		paddingHorizontal: 10
-	}
+    borderRadius: 10
+  },
+  titleButton: {
+    // fontSize: 12,
+    // color: '#FFFFFF',
+    // paddingHorizontal: 10
+
+    fontSize: Style.FONT_SIZE_SMALLER,
+    paddingHorizontal: 10,
+    paddingVertical: 3,
+    color: '#FFFFFF',
+    textAlign: 'center'
+  }
 };
 
 export default HeaderDetail;
