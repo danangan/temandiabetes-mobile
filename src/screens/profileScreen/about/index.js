5b3109a3257970f7939cf7f0fe5c011dca1202ab
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-27 01:00:21 +0700

    changes text tentang wording

--- src/screens/profileScreen/about/index.js
+++ src/screens/profileScreen/about/index.js
@@ -10,34 +10,24 @@ const AboutScreen = props => (
 		<NavigationBar onPress={() => props.navigator.pop()} title={'TENTANG'} />
 		<ScrollView>
 			<Text style={styles.textStyle}>
-				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-				been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-				galley of type and scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-				It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
-				passages, and more recently with desktop publishing software like Aldus PageMaker including
-				versions of Lorem Ipsum. {'\n'}
+				Teman Diabetes adalah aplikasi yang memudahkan diabetisi, keluarga dan 
+				ahli medis bekerjasama untuk memastikan diabetisi hidup lebih sehat.  
+				Terhubung dengan DNurse, sebuah alat kesehatan untuk mengukur gula darah, 
+				Teman Diabetes mampu mengukur gula darah secara otomatis dan menganalisa 
+				kesehatan diabetisi dengan cepat. Aplikasi Teman Diabetes siap menemani 
+				diabetisi dan keluarga sejak pertama kali didiagnosa. 
+				Teman Diabetes menjawab kekhawatiran diabetisi mengenai cara hidup sehat 
+				dan nyaman dengan diabetes, mendapatkan informasi terpercaya, 
+				serta mencari dukungan sesama diabetisi. {'\n'}
 				{'\n'}
-				Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece
-				of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,
-				a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure
-				Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the
-				word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from
-				sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
-				Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very
-				popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
-				amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since
-				the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de
-				Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form,
-				accompanied by English versions from the 1914 translation by H. Rackham.{'\n'}
-				{'\n'}
-				It is a long established fact that a reader will be distracted by the readable content of a
-				page when looking at its layout. The point of using Lorem Ipsum is that it has a
-				more-or-less normal distribution of letters, as opposed to using 'Content here, content
-				here', making it look like readable English. Many desktop publishing packages and web page
-				editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will
-				uncover many web sites still in their infancy. Various versions have evolved over the years,
-				sometimes by accident, sometimes on purpose (injected humour and the like).
+				
+				Pengguna dapat belajar cara hidup sehat dan nyaman dengan diabetes dengan terus 
+				memonitor gula darah, makanan yang dikonsumsi, kegiatan yang dilakukan, 
+				serta obat-obatan yang diminum. Informasi ini juga dapat dikirim ke keluarga, maupun 
+				ahli medis sesuai dengan kebutuhan. Via fitur artikel, Teman Diabetes memberikan 
+				informasi terpercaya yang telah diverifikasi dokter. Dukungan juga bisa didapatkan 
+				secara maya dengan ikut serta di forum Teman Diabetes yang dimoderasi ahli medis. 
+				Sebaliknya, pengguna juga bisa ikut serta event-event yang terdaftar di fitur event. 
 			</Text>
 		</ScrollView>
 	</View>
@@ -50,9 +40,9 @@ const styles = {
 		padding: Style.PADDING
 	},
 	textStyle: {
+		textAlign: 'auto',
 		fontFamily: 'OpenSans-Regular',
 		fontSize: Style.FONT_SIZE_SMALL,
-		textAlign: 'justify',
 		paddingTop: Style.PADDING
 	}
 };
