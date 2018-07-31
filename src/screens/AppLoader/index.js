77d440e6300b127622e1e8cb627b2c8c84afa2a9
Danang Arbansa Nur <danang.arbansa@gmail.com>
2018-07-31 15:02:01 +0700

    fix  login fb and google stuck in loading

--- src/screens/AppLoader/index.js
+++ src/screens/AppLoader/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import { Linking, AsyncStorage } from 'react-native';
+import { Linking, AsyncStorage, Alert } from 'react-native';
 import { connect } from 'react-redux';
 import firebase from 'react-native-firebase';
+import { Navigation } from 'react-native-navigation';
+import { result } from 'lodash';
 
 import { Spinner } from '../../components/Spinner';
 import { authToken } from '../../utils/constants';
 import { API_CALL } from '../../utils/ajaxRequestHelper';
 import { updateDeepLink } from '../../actions';
 import { mainApp, startApp, startLoginPage } from '../../../App';
-
+import { resetState } from '../../actions/loginActions';
 class AppLoader extends Component {
   constructor(props) {
     super(props);
@@ -26,6 +28,37 @@ class AppLoader extends Component {
     });
 
     this.firebaseHandler();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // the inactive user goes here
+    // here we check if the inactive user is a new user or not
+    // if the user is existing user, the isnewuser value will be false, thus
+    // we give them alert that the user account is inactive
+    if (nextProps.loginReducer.isNewUser === true) {
+      // redirect to fourth page
+      Navigation.startSingleScreenApp({
+        screen: {
+          screen: 'TemanDiabetes.RegisterScreenFourth',
+          navigatorStyle: {
+            navBarHidden: true
+          }
+        },
+        passProps: {
+          email: nextProps.loginReducer.email,
+          _id: nextProps.loginReducer.currentUser._id,
+          nama: nextProps.loginReducer.nama,
+          registerType: 'GoogleSignIn'
+        },
+        animationType: 'fade'
+      });
+    } else if (result(nextProps.loginReducer, 'currentUser.is_active') === false) {
+      Alert.alert(
+        'Akun Anda sedang tidak aktif.',
+        'Hubungi penyedia layanan untuk info lebih lanjut.'
+      );
+      this.props.resetState();
+    }
   }
 
   async initMainApp(cb = () => {}) {
@@ -99,8 +132,13 @@ class AppLoader extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  loginReducer: state.loginReducer
+});
+
 const mapDispatchToProps = dispatch => ({
-  updateDeepLink: deepLink => dispatch(updateDeepLink(deepLink))
+  updateDeepLink: deepLink => dispatch(updateDeepLink(deepLink)),
+  resetState: () => dispatch(resetState()),
 });
 
 export default connect(
