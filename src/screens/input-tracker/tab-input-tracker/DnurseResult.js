b71413e41ca3661c62bdaef96f187ef6e322d9f5
Eri Irawan <eryirawan91@gmail.com>
2018-08-02 21:26:27 +0700

    uncomment detection dnurse

--- src/screens/input-tracker/tab-input-tracker/DnurseResult.js
+++ src/screens/input-tracker/tab-input-tracker/DnurseResult.js
@@ -24,7 +24,7 @@ class DnurseResult extends React.Component {
   }
 
   componentDidMount() {
-    // this.requestAudioPermission();
+    this.requestAudioPermission();
   }
 
   onNavigation = () => {
