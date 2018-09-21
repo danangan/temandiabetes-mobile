1c15498b86abcee2658a810bfed8a8ef8df37f72
Danang Arbansa Nur <danang.arbansa@gmail.com>
2018-09-22 00:37:56 +0700

    deletee upload from fb option

--- src/components/imageUploader/index.js
+++ src/components/imageUploader/index.js
@@ -9,9 +9,6 @@ import { updateProfile } from '../../actions/profileActions'
 
 const imagePickerOption = {
   title: 'Select Avatar',
-  // customButtons: [
-  //   {name: 'fb', title: 'Choose Photo from Facebook'},
-  // ],
   storageOptions: {
     skipBackup: true,
     path: 'images'
