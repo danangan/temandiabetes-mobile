da3d014274f821bb85ae7c46edebe7abba4924a3
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-09-22 00:37:04 +0700

    take out Choose Photo from Facebook

--- src/components/imageUploader/index.js
+++ src/components/imageUploader/index.js
@@ -9,9 +9,9 @@ import { updateProfile } from '../../actions/profileActions'
 
 const imagePickerOption = {
   title: 'Select Avatar',
-  customButtons: [
-    {name: 'fb', title: 'Choose Photo from Facebook'},
-  ],
+  // customButtons: [
+  //   {name: 'fb', title: 'Choose Photo from Facebook'},
+  // ],
   storageOptions: {
     skipBackup: true,
     path: 'images'
