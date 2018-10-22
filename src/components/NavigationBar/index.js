4a51c1968b302152e2b0c1f23ef13cd40e5901c3
Eri Irawan <eryirawan91@gmail.com>
2018-10-22 22:59:45 +0700

    passing container style

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -4,50 +4,50 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 import Style from '../../style/defaultStyle';
 import color from '../../style/color';
 
-const NavigationBar = ({ onPress, title }) => (
-	<View style={styles.containerStyle}>
-		<TouchableOpacity style={styles.backButtonStyle} onPress={onPress}>
-			<Image
-				resizeMode={'contain'}
-				style={styles.imageStyle}
-				tintColor={color.red}
-				source={require('../../assets/icons/back.png')}
-			/>
-		</TouchableOpacity>
-		<View style={styles.titleContainerStyle}>
-			<Text style={styles.titleStyle}>{title}</Text>
-		</View>
-	</View>
+const NavigationBar = ({ onPress, title, containerStyle, backButtonStyle }) => (
+  <View style={[styles.containerStyle, containerStyle]}>
+    <TouchableOpacity style={[styles.backButtonStyle, backButtonStyle]} onPress={onPress}>
+      <Image
+        resizeMode={'contain'}
+        style={styles.imageStyle}
+        tintColor={color.red}
+        source={require('../../assets/icons/back.png')}
+      />
+    </TouchableOpacity>
+    <View style={styles.titleContainerStyle}>
+      <Text style={styles.titleStyle}>{title}</Text>
+    </View>
+  </View>
 );
 
 const styles = {
-	containerStyle: {
+  containerStyle: {
     flexDirection: 'row'
-	},
-	backButtonStyle: {
+  },
+  backButtonStyle: {
     paddingLeft: 10,
-		justifyContent: 'flex-start',
-		alignItems: 'flex-start',
-	},
-	imageStyle: {
-		width: 25,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start'
+  },
+  imageStyle: {
+    width: 25,
     height: 25,
     tintColor: color.red
-	},
-	titleContainerStyle: {
-		flex: 1,
-		justifyContent: 'flex-start',
-		alignItems: 'flex-start',
-	},
-	titleStyle: {
+  },
+  titleContainerStyle: {
     flex: 1,
-		fontSize: Style.FONT_SIZE,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start'
+  },
+  titleStyle: {
+    flex: 1,
+    fontSize: Style.FONT_SIZE,
     fontFamily: 'Montserrat-Regular',
     textAlign: 'center',
     alignSelf: 'center',
     paddingRight: 25,
     color: color.red
-	}
+  }
 };
 
 export { NavigationBar };
